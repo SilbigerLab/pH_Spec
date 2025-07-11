d5a041e98c7db698b14ca99a18e473061a4d5d9b
Nyssa Silbiger <silbiger@hawaii.edu>
2025-07-10 16:53:42 -1000

    update chl script

--- Scripts/ChlSpecScript.R
+++ Scripts/ChlSpecScript.R
@@ -17,50 +17,21 @@ foldername<-'test_chl' # folder of the day
 filename<-'testchl2.csv' # data
 sampleID<-'Chl_template_test.csv' # template of sample IDs
 platename<-'plate1_test' # this will be the name of your file
+metadata_file<-"testchl2_metadata.csv" # file with slurry vols and SA
 
 ## What is the path length?
 PL<-0.71 # pathlength for donahue lab plate
 
-## Temp and Salinity ----------
-#Temeperataure pH was run at IN THE LAB
-Temperature<-22.9
-Salinity<-34 # note if you have a range of salinities then import a file with all salinities and modify script below (add a column with sample IDs and left_join with AllData Df)
-
-### Temperature in situ (either in the aquarium or in the field)#####
-## If you want to calculate insitu pH then enter TRUE in the statement below and the filename with the temperature files, if not enter FALSE
-CalculateInSitu<-FALSE
-TempInSituFileName<-'TempInSitu.csv' #file name of the in situ temperatures. (enter the sample IDs identical to the pH sample ID template)  
-
-# Change slope and intercept with each new batch of dye --------------
-# Dye created by: Jenn Fields
-# Date created: 7-08-2022
-dye_intercept<-14.504
-dye_slope<--0.5663
-
-# Change slope and intercept with each new batch of dye --------------
-# Dye created by: Jenn Fields
-# Slope recreated by Robert Dellinger
-# Date created: 8-09-2022
-#dye_intercept<-18.825
-#dye_slope<--0.581
-
-# Second Dye created by Deme Panos
-# Use the following intercept and slope for Moorea spec runs
-#dye_intercept<-14.653
-#dye_slope<--0.5694
 
 # DONT CHANGE ANYTHING BELOW HERE ----------------------------------
-
-# read in the in situ temp data if true
-if(CalculateInSitu==TRUE){
-  TempInSitu<-read.csv(paste0('Data/',foldername,'/',TempInSituFileName))
-}
-
 
 # Read in the Sample IDs
 sampleIDNames<-read_csv(here('Data',foldername,sampleID))
 sampleIDNames<-sampleIDNames %>%
   select(Well.Location, Sample.Name) # pull out the location and the IDs
+
+# read in metadata
+metadata<-read_csv(here("Data", foldername, metadata_file))
 
 ### format the 96 well plate data ################
 #read in the rows w/o the dye
@@ -98,50 +69,37 @@ AllData<-AllData %>%
 
 ### Run pH Analysis ##################
 # chl from Jeffry and Humphreys
-ChlData<-AllData %>%
+ChlData_raw<-AllData %>%
   mutate(chla = 11.43*(ChlPlate_663 - ChlPlate_750 / PL) - 0.64*(ChlPlate_630 - ChlPlate_750/PL),
          chlc = 27.09*(ChlPlate_630 - ChlPlate_750 / PL) - 3.63*(ChlPlate_663 - ChlPlate_750/PL),
          Totalchl = chla+chlc,
            daterun = Sys.Date()) %>%
   left_join(sampleIDNames) %>% # join with the sampleIDs
-  drop_na(Sample.Name)
+  drop_na(Sample.Name) %>%
+  left_join(metadata) %>%
+  mutate(chla_ug_cm2 = (chla*Slurry_vol_ml)/SA_cm2,  # normalize to coral slurry and SA
+         chlc_ug_cm2 = (chlc*Slurry_vol_ml)/SA_cm2,
+         Totalchl_ug_cm2 = (Totalchl*Slurry_vol_ml)/SA_cm2)
+         
 
 # averaged data by sample ID
-ChlData_ave<-ChlData %>%
+ChlData_ave<-ChlData_raw %>%
   group_by(Sample.Name) %>%
-  summarise_at(vars(chla:Totalchl), .funs = list(function(x){mean(x, na.rm = TRUE)},
+  summarise_at(vars(chla:Totalchl, chla_ug_cm2:Totalchl_ug_cm2), .funs = list(function(x){mean(x, na.rm = TRUE)},
                                                  function(x){sd(x, na.rm = TRUE)}/sqrt(n()))) %>%
   rename(chla_mean = chla_fn1, chlc_mean=chlc_fn1, TotalChl_mean = Totalchl_fn1,
-         chla_SE = chla_fn2, chlc_SE = chlc_fn2, Totalchl_SE = Totalchl_fn2) %>%
-  select(Sample.Name,chla_mean,chla_SE,chlc_mean,chlc_SE,TotalChl_mean,Totalchl_SE) # put in a better order
+         chla_SE = chla_fn2, chlc_SE = chlc_fn2, Totalchl_SE = Totalchl_fn2,
+         chla_ug_cm2_mean = chla_ug_cm2_fn1, chlc_ug_cm2_mean = chlc_ug_cm2_fn1,
+         Totalchl_ug_cm2_mean = Totalchl_ug_cm2_fn1, 
+         chla_ug_cm2_SE = chla_ug_cm2_fn2, chlc_ug_cm2_SE = chlc_ug_cm2_fn2,
+         Totalchl_ug_cm2_SE = Totalchl_ug_cm2_fn2) %>%
+  select(Sample.Name,chla_mean,chla_SE,chlc_mean,chlc_SE,TotalChl_mean,Totalchl_SE,
+         chla_ug_cm2_mean,chla_ug_cm2_SE, chlc_ug_cm2_mean,chlc_ug_cm2_SE,Totalchl_ug_cm2_mean,Totalchl_ug_cm2_SE ) # put in a better order
   
 
-### rename the columns to be easier!
-
-# run this block if calculating in situ pH
-  if(CalculateInSitu==TRUE){
-    pHData<-pHData %>%
-      mutate(pHinsitu = pHinsi(pH = pH_in_lab, Tinsi = TempInSitu, Tlab = Temperature, pHscale = "T")) # calculate insitu pH
-  }
 
 ### Export the data
 ## all the info
-write.table(pHData,paste0("Data/",foldername,"/pHoutputFull",platename,".csv"),sep=",", row.names=FALSE)
-
-## only the pH data with means and SE of triplicates
-if(CalculateInSitu==TRUE){
-  pHData %>% 
-  select(Sample.Name, pHinsitu)%>% 
-  group_by(Sample.Name)%>%
-  summarise(pHmean = mean(pHinsitu, na.rm=TRUE), SE = sd(pHinsitu, na.rm=TRUE)/n())%>%
-  write.table(.,paste0("Data/",foldername,"/pH_simple",platename,".csv"),sep=",", row.names=FALSE)
-}
-
-if(CalculateInSitu==FALSE){
-  pHData %>% 
-    select(Sample.Name, pH_in_lab)%>% 
-    group_by(Sample.Name)%>%
-    summarise(pHmean = mean(pH_in_lab, na.rm=TRUE), SE = sd(pH_in_lab, na.rm=TRUE)/n())%>%
-    write.table(.,paste0("Data/",foldername,"/pH_simple",platename,".csv"),sep=",", row.names=FALSE)
-}
+write_csv(ChlData_ave, here("Data", foldername, paste0(platename,"_summary.csv"))) # print the summary data
+write_csv(ChlData_raw, here("Data", foldername, paste0(platename,"_raw.csv"))) # print the raw data
 
